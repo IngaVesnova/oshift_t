30e046acf9663dc1b629c7142569f0fa6519594e
Inga Vesnova <inga.vesnova@gmail.com>
2015-11-20 16:28:25 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+} // handler
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
+} // main
